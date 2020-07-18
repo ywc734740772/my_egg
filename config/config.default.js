08fcc2dc1a0d3bd2acb769bf48f2acdbe00e9bb0
Tony <wenchao.yang@poi-t.com>
2020-07-18 09:12:01 +0800

    123

--- config/config.default.js
+++ config/config.default.js
@@ -50,13 +50,18 @@ module.exports = appInfo => {
             host: 'localhost',
             port: '3306',
             user: 'root',
-            password: '',
+            password: '123456',
             database:'egg_sql',
         },
         // 是否加载到 app 上，默认开启
         app: true,
         // 是否加载到 agent 上，默认关闭
         agent: false,
+    };
+
+    config.cors = {
+        origin:'*',
+        allowMethods: 'GET,PUT,POST,DELETE'
     };
 
     return {
--- config/plugin.js
+++ config/plugin.js
@@ -16,3 +16,8 @@ exports.mysql = {
     enable: true,
     package: 'egg-mysql',
 };
+
+exports.cors = {
+    enable: true,
+    package: 'egg-cors',
+};
